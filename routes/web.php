5b7f61f76065d80adde452b761a22224c27ac562
Labib Makarim <labibmakarim197@gmail.com>
2024-11-04 09:55:58 +0700

    template, dashboard

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Livewire\Dashboard;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/dashboard', Dashboard::class)->name('dashboard');
